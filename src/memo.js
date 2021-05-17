a5332009ed03e3b3930b110c278c62f35b1f2d84
tuna <tuna2@smartosc.com>
2021-05-17 17:59:55 +0700

    Update files

---
+++ src/memo.js
@@ -0,0 +1,20 @@
+
+import React, { memo, useCallback } from "react";
+
+function Logout({ username, onLogout }) {
+    console.log("========render logout========");
+    return <div onClick={onLogout}>Logout {username}</div>;
+}
+
+const MemoizedLogout = memo(Logout);
+
+export default function UseMemo() {
+    // const onLogout = useCallback(() => cookies.clear(), [cookies]);
+    return (
+        <>
+            <MemoizedLogout username="truong" onLogout={() => {}} />
+        </>
+    );
+}
+
+// refer: https://dmitripavlutin.com/use-react-memo-wisely/
